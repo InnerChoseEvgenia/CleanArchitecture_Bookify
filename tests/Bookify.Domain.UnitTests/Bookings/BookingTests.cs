a5b13aee968107af90f291da969952117625ecf8
InnerChoseEvgenia <evgenialukashen@gmail.com>
2026-02-14 20:22:50 +0300

    Added UnitTests for Domain layer using xUnit

---
+++ tests/Bookify.Domain.UnitTests/Bookings/BookingTests.cs
@@ -0,0 +1,33 @@
+﻿using Bookify.Domain.UnitTests.Apartments;
+using Bookify.Domain.UnitTests.Infrastructure;
+using Bookify.Doman.Bookings.Events;
+using Bookify.Doman.Bookings;
+using Bookify.Doman.Shared;
+using Bookify.Doman.Users;
+using Bookify.Domain.UnitTests.User;
+using Shouldly;
+
+namespace Bookify.Domain.UnitTests.Bookings;
+
+public class BookingTests : BaseTest
+{
+    [Fact]
+    public void Reserve_Should_RaiseBookingReservedDomainEvent()
+    {
+        // Arrange
+
+        var user = Bookify.Doman.Users.User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
+        var price = new Money(10.0m, Currency.Usd);
+        var duration = DateRange.Create(new DateOnly(2024, 1, 1), new DateOnly(2024, 1, 10));
+        var apartment = ApartmentData.Create(price);
+        var pricingService = new PricingService();
+
+        // Act
+        var booking = Booking.Reserve(apartment, user.Id, duration, DateTime.UtcNow, pricingService);
+
+        // Assert
+        var bookingReservedDomainEvent = AssertDomainEventWasPublished<BookingReservedDomainEvent>(booking);
+
+        bookingReservedDomainEvent.BookingId.ShouldBe(booking.Id);
+    }
+}
---
+++ tests/Bookify.Domain.UnitTests/User/UserTests.cs
@@ -0,0 +1,34 @@
+﻿using Bookify.Domain.UnitTests.Infrastructure;
+using Bookify.Doman.Users.Events;
+using Bookify.Doman.Users;
+using System.Data;
+using Shouldly;
+
+namespace Bookify.Domain.UnitTests.User;
+
+public class UserTests : BaseTest
+{
+    [Fact]
+    public void Create_Should_SetPropertyValue()
+    {
+        // Act
+        var user = Bookify.Doman.Users.User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
+
+        // Assert
+        user.FirstName.ShouldBe(UserData.FirstName);
+        user.LastName.ShouldBe(UserData.LastName);
+        user.Email.ShouldBe(UserData.Email);
+    }
+
+    [Fact]
+    public void Create_Should_RaiseUserCreatedDomainEvent()
+    {
+        // Act
+        var user = Bookify.Doman.Users.User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
+
+        // Assert
+        var userCreatedDomainEvent = AssertDomainEventWasPublished<UserCreatedDomainEvent>(user);
+
+        userCreatedDomainEvent.UserId.ShouldBe(user.Id);
+    }
+}
